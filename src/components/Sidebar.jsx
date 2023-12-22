e6d97fc93ad8a7e41f9aaf09efc7fba43389903f
sameed02 <sameedahmad3@gmail.com>
2023-12-23 02:24:22 +0530

    built static Layout for AppLayout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,16 @@
-import Map from "./Map";
-import Logo from "./Logo";
+import Logo from "../components/logo";
 
 import styles from "./Sidebar.module.css";
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
-      <Map />
+      <p>list of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
     </div>
   );
 }
