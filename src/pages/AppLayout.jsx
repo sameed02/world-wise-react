0ddbe1bd59a00fe8774465aa3f7d9841400a5e1d
sameed02 <sameedahmad3@gmail.com>
2023-12-30 02:05:59 +0530

    app is feature complete, added fake authentication

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,16 @@
 import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
 import styles from "./AppLayout.module.css";
+import User from "../components/User";
+/* import { useAuth } from "../contexts/FakeAuthContext"; */
 
 function AppLayout() {
+  /*  const { user } = useAuth(); */
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
